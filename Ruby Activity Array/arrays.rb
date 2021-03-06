c6a77562579c7f813886fa95575166936ffd467e
AlexCitrus <kidong044@gmail.com>
2021-03-06 11:42:47 +0800

    third

--- Ruby Activity Array/arrays.rb
+++ Ruby Activity Array/arrays.rb
@@ -3,14 +3,14 @@ b = [3, 1, 2, 1, 3, 2]
 c = [2, 4, 3, 5, 1]
 
 def first_duplicate(a)
-    counter = []
+    counts = []
     a.each do |value|
-        if counter[value]
+        if counts[value]
             return value
         end
-        counter[value] = true
+        counts[value] = true
     end
-    -1
+    return -1
 end
 
 puts first_duplicate(a) # Output: 3
