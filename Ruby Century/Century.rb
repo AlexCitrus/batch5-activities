472a43c6fc22fd2c2f5fe6b111264695e24c6703
AlexCitrus <kidong044@gmail.com>
2021-03-16 19:29:05 +0800

    third commit

--- Ruby Century/Century.rb
+++ Ruby Century/Century.rb
@@ -3,7 +3,7 @@ def century_from_year(year)
       year % 100 == 0 ? year / 100 : (year - (year % 100)) / 100 + 1
 end
 
-puts century_from_year(1705)
-puts century_from_year(1900)
-puts century_from_year(1601)
-puts century_from_year(2000)
+puts " => #{century_from_year(1705)}"
+puts " => #{century_from_year(1900)}"
+puts " => #{century_from_year(1601)}"
+puts " => #{century_from_year(2000)}"
